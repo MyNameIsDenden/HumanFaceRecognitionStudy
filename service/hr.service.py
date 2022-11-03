18b280edcc71c4fd14b47dfea84e184ba6c57520
denden <13718220410@163.com>
2022-11-03 16:03:09 +0800

    提交

--- service/hr.service.py
+++ service/hr.service.py
@@ -170,5 +170,75 @@ def get_month_report(month):
 
     for emp in o.EMPLOYEES:
         emp_lock_data = []
-        
+        if emp.name in o.LOCK_RECORD.keys():
+            emp_lock_list = o.LOCK_RECORD.get(emp.name)
+            index_day = month_first_day
+            while index_day <= month_last_day:
+                is_absent = True
+                earliest_time = datetime.datetime.strptime(index_day + "00:00:00", "%Y-%m-%d %H-%M-%S")
+                noon_time = datetime.datetime.strptime(index_day + "12:00:00", "%Y-%m-%d %H-%M-%S")
+                latest_time = datetime.datetime.strptime(index_day + "23:59:59", "%Y-%m-%d %H-%M-%S")
+                work_time = datetime.datetime.strptime(index_day + "" + o.WORK_TIME, "%Y-%m-%d %H-%M-%S")
+                closing_time = datetime.datetime.strptime(index_day + "" + o.CLOSING_TIME, "%Y-%m-%d %H-%M-%S")
+                emp_today_data = ""
+                for lock_time_str in emp_lock_list:
+                    lock_time = datetime.datetime.strptime(lock_time_str, "%Y-%m-%d %H-%M-%S")
+                    if earliest_time < lock_time < latest_time:
+                        is_absent = False
+                        if lock_time <= work_time:
+                            emp_today_data += clock_in
+                        elif lock_time >= closing_time:
+                            emp_today_data += clock_out
+                        elif work_time < lock_time <= noon_time:
+                            emp_today_data += late
+                        elif noon_time < lock_time < closing_time:
+                            emp_today_data += left_early
+                if is_absent:
+                    emp_today_data = absent
+                emp_lock_data.append(emp_today_data)
+                index_day = index_day + datetime.timedelta(days=1)
+        else:
+            index_day = month_first_day
+            while index_day <=month_last_day:
+                emp_lock_data.append(absent)
+                index_day = index_day + datetime.timedelta(days=1)
+        lock_report[emp.name]=emp_lock_data
+
+    report = "\"姓名/日期\""
+    index_day = month_first_day
+    while index_day <= month_last_day:
+        report += ",\"" + str(index_day) + "\""
+        index_day = index_day + datetime.timedelta(days=1)
+    report += "\n"
+
+    for emp in lock_report.keys():
+        report += "\"" + emp + "\""
+        date_list = lock_report.get(emp)
+        for data in date_list:
+            text = ""
+            if absent == data:
+                text = "【缺席】"
+            elif clock_in in data and clock_out in data:
+                text = ""
+            else:
+                if late in data and clock_in not in data:
+                    text += "【迟到】"
+                if left_early in data and clock_out not in data:
+                    text += "【早退】"
+                if clock_out not in data and left_early not in data:
+                    text += "【下班未打卡】"
+                if clock_in not in data and late not in data:
+                    text += "【上班未打卡】"
+            report += ",\"" + text + "\""
+        report += "\n"
+
+    title_date = month_first_day.strftime("%Y{y}%m{m}").format(y="年", m="月")
+    file_name = title_date + "考勤月报"
+    io.create_CSV(file_name, report)
+
+def get_pre_month_report():
+    today = datetime.date.today()
+    pre_month_first_day = datetime.date(today.year, today.month-1, 1)
+    pre_month = pre_month_first_day.strftime("%Y-%m")
+    get_month_report(pre_month)
 
---
+++ service/recognize_service.py
@@ -0,0 +1,23 @@
+import cv2
+import numpy as np
+import os
+
+RECOGINZER = cv2.face.LBPHFaceRecognizer_create()
+PASS_CONF = 45
+FACE_CASCADE = cv2.CascadeClassifier(os.getcwd() + "\\cascades\\haarcascade_frontalface_default.xml")
+
+
+def train(photos, lables):
+    RECOGINZER.train(photos, np.array(lables))
+
+
+def found_face(gary_img):
+    faces = FACE_CASCADE.detectMultiScale(gary_img, 1.15, 4)
+    return len(faces) > 0
+
+
+def recognise_faces(photos):
+    label, confidence = RECOGINZER.predict(photos)
+    if confidence > PASS_CONF:
+        return -1
+    return label
